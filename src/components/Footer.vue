4b1a4ea23ea4b24052fd0e1e2c50f005dbf20246
vando <vandorramos@gmail.com>
2023-12-08 10:37:28 -0300

    Set header and footer layouts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Footer</p>
+  <footer id="footer">
+    <p>Make Your Burger &copy; 2023</p>
+  </footer>
 </template>
 
 <script>
@@ -9,5 +11,14 @@ export default {
 </script>
 
 <style scoped>
-
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
 </style>
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Orders</h1>
+    <h1 class="main-container">Orders</h1>
   </div>
 </template>
