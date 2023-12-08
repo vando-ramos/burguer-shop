669fba80e68a2e4e35767d02ad2cb6e901d389dd
vando <vandorramos@gmail.com>
2023-12-08 09:54:40 -0300

    Set routes and components for home and orders

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img src="" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/orders">Orders</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
